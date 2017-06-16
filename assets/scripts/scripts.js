fd01f9c6b880f9a2d5eeece566078eb39673ea39
Merilee Wheelock <eelirem@gmail.com>
2017-06-16 14:14:59 -0400

    Fixed scrolling and view height issues

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
+
+	$('.nav-item').click(function(){
+		var currentNavbarId = $('.nav-active').attr('id');
+		// console.log("currentNavbarId: " + currentNavbarId);
+
+		var targetedPageId = event.target.id;
+		// console.log("targetedPageId: " + targetedPageId);
+
+		$('.pages').css({
+			'top':`-${targetedPageId - 1}00vh`
+		});
+
+ 		$(`#${currentNavbarId}`).removeClass('nav-active');
+		$(`#${targetedPageId}`).addClass('nav-active');
+	});
+
+
 	// Page 1 to 2
 	$('.down-arrow.pg-one').click(function(){
-		$('.page1').css({
+		$('.pages').css({
 			'top':'-100vh'
-		});
-		$('.page2').css({
-			'top':'0vh'
 		});
 		$('.nav-welcome').removeClass('nav-active');
 		$('.nav-about').addClass('nav-active');
 	});
 
 	$('.up-arrow.pg-two').click(function(){
-		$('.page1').css({
+		$('.pages').css({
 			'top':'0vh'
-		});
-		$('.page2').css({
-			'top':'100vh'
 		});
 		$('.nav-welcome').addClass('nav-active');
 		$('.nav-about').removeClass('nav-active');
@@ -24,22 +35,16 @@ $(document).ready(function(){
 
 	// Page 2 to 3
 	$('.down-arrow.pg-two').click(function(){
-		$('.page2').css({
-			'top':'-100vh'
-		});
-		$('.page3').css({
-			'top':'0vh'
+		$('.pages').css({
+			'top':'-200vh'
 		});
 		$('.nav-about').removeClass('nav-active');
 		$('.nav-skills').addClass('nav-active');
 	});
 
 	$('.up-arrow.pg-three').click(function(){
-		$('.page2').css({
-			'top':'0vh'
-		});
-		$('.page3').css({
-			'top':'100vh'
+		$('.pages').css({
+			'top':'-100vh'
 		});
 		$('.nav-about').addClass('nav-active');
 		$('.nav-skills').removeClass('nav-active');
@@ -47,22 +52,16 @@ $(document).ready(function(){
 
 	//Page 3 to 4
 	$('.down-arrow.pg-three').click(function(){
-		$('.page3').css({
-			'top':'-100vh'
-		});
-		$('.page4').css({
-			'top':'0vh'
+		$('.pages').css({
+			'top':'-300vh'
 		});
 		$('.nav-skills').removeClass('nav-active');
 		$('.nav-portfolio').addClass('nav-active');
 	});
 
 	$('.up-arrow.pg-four').click(function(){
-		$('.page3').css({
-			'top':'0vh'
-		});
-		$('.page4').css({
-			'top':'100vh'
+		$('.pages').css({
+			'top':'-200vh'
 		});
 		$('.nav-skills').addClass('nav-active');
 		$('.nav-portfolio').removeClass('nav-active');
@@ -70,22 +69,16 @@ $(document).ready(function(){
 
 	//Page 4 to 5
 	$('.down-arrow.pg-four').click(function(){
-		$('.page4').css({
-			'top':'-100vh'
-		});
-		$('.page5').css({
-			'top':'0vh'
+		$('.pages').css({
+			'top':'-400vh'
 		});
 		$('.nav-portfolio').removeClass('nav-active');
 		$('.nav-contact').addClass('nav-active');
 	});
 
 	$('.up-arrow.pg-five').click(function(){
-		$('.page4').css({
-			'top':'0vh'
-		});
-		$('.page5').css({
-			'top':'100vh'
+		$('.pages').css({
+			'top':'-300vh'
 		});
 		$('.nav-portfolio').addClass('nav-active');
 		$('.nav-contact').removeClass('nav-active');
